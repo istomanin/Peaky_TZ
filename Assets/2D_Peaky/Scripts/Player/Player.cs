d02b036034dfb4fc9451fc735dd54a4b4ed4d3ee
istomanin <istomanin@gmail.com>
2026-06-06 18:52:09 +0600

    MapLimit added
    Добавл ограничение передвижения персонажа, при достижении определенного значения, игрок перестает двигаться, постарался реазизовать своего рода защиту от неправильного ввода значений

--- Assets/2D_Peaky/Scripts/Player/Player.cs
+++ Assets/2D_Peaky/Scripts/Player/Player.cs
@@ -5,26 +5,28 @@ using UnityEngine;
 [SelectionBase]
 public class Player : MonoBehaviour
 {
+
+    [SerializeField] private float MapLimit = 12.5f;
     public static Player Instance;
-    
-  
+
+
 
     [SerializeField] private float movingSpeed = 5f;
-   
+
     private Rigidbody2D _rb;
-  
+
 
     private Vector2 _inputVector;
-  
+
 
     private void Awake()
     {
         _rb = GetComponent<Rigidbody2D>();
-      
+
         Instance = this;
-       
+
     }
-  
+
 
     private void Update()
     {
@@ -34,13 +36,21 @@ public class Player : MonoBehaviour
     private void FixedUpdate()
     {
         HandleMovment();
+
     }
 
-   
+
     private void HandleMovment()
     {
+         Vector2 targetPosition =_rb.position +_inputVector *(movingSpeed * Time.fixedDeltaTime);
 
-        _rb.MovePosition(_rb.position + _inputVector * (movingSpeed * Time.fixedDeltaTime));
+        targetPosition.x = Mathf.Clamp(targetPosition.x, -MapLimit, MapLimit);
+
+        targetPosition.y = Mathf.Clamp(targetPosition.y, -MapLimit, MapLimit);
+
+        _rb.MovePosition(targetPosition);
 
     }
+
+
 }
